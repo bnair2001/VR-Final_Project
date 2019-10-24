d07783e160b9ac01fd9ded289566ea0d3eaae62c
Bharath Nair <bharath.rajeevnair@student.adelaide.edu.au>
2019-10-24 15:04:11 +1030

    object shake

---
+++ Assets/ObjectShake.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// http://www.mikedoesweb.com/2012/camera-shake-in-unity/
+
+public class ObjectShake : MonoBehaviour {
+
+	private Vector3 originPosition;
+	private Quaternion originRotation;
+	public float shake_decay = 0.0002f;
+	public float shake_intensity = .03f;
+	public Vector3 position;
+	int rand;
+	private float temp_shake_intensity = 0;
+	bool vib = true;
+	bool loop = true;
+	
+	void OnGUI (){
+		
+	}
+		
+	void Update (){
+			Shake ();
+		rand = Random.Range(1, 100);
+		if (temp_shake_intensity > 0){
+			transform.position = originPosition + Random.insideUnitSphere * temp_shake_intensity;
+			transform.rotation = new Quaternion(
+				originRotation.x + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
+				originRotation.y + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
+				originRotation.z + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
+				originRotation.w + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f);
+			temp_shake_intensity -= shake_decay;
+		}
+	}
+	
+	void Shake(){
+		originPosition = transform.position;
+		originRotation = transform.rotation;
+		temp_shake_intensity = shake_intensity;
+
+	}
+}
