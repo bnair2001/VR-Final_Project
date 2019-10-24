30193466fbecb15536c25dd49a89bad2090f8fc5
Bharath Nair <bharath.rajeevnair@student.adelaide.edu.au>
2019-10-24 19:56:49 +1030

    children shake

--- Assets/ObjectShake.cs
+++ Assets/ObjectShake.cs
@@ -1,14 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-/// http://www.mikedoesweb.com/2012/camera-shake-in-unity/
-
 public class ObjectShake : MonoBehaviour {
 
 	private Vector3 originPosition;
 	private Quaternion originRotation;
-	public float shake_decay = 0.0002f;
-	public float shake_intensity = .03f;
+	public float shake_decay = 0.019f;
+	public float shake_intensity = 0.011f;
 	public Vector3 position;
 	int rand;
 	private float temp_shake_intensity = 0;
@@ -20,8 +17,10 @@ public class ObjectShake : MonoBehaviour {
 	}
 		
 	void Update (){
+			rand = Random.Range(1, 100);
+			if (rand < 50) {
 			Shake ();
-		rand = Random.Range(1, 100);
+			}
 		if (temp_shake_intensity > 0){
 			transform.position = originPosition + Random.insideUnitSphere * temp_shake_intensity;
 			transform.rotation = new Quaternion(
@@ -29,14 +28,13 @@ public class ObjectShake : MonoBehaviour {
 				originRotation.y + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
 				originRotation.z + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f,
 				originRotation.w + Random.Range (-temp_shake_intensity,temp_shake_intensity) * .2f);
-			temp_shake_intensity -= shake_decay;
+				temp_shake_intensity -= shake_decay;
 		}
 	}
 	
 	void Shake(){
-		originPosition = transform.position;
-		originRotation = transform.rotation;
-		temp_shake_intensity = shake_intensity;
-
+			originPosition = transform.position;
+			originRotation = transform.rotation;
+			temp_shake_intensity = shake_intensity;
 	}
 }
